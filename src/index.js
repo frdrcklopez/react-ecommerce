f0abe4a01196eee1972fbcbc29e4b65dc01b8b42
effs12 <fredericklopez12@gmail.com>
2022-08-21 04:26:03 -0400

    Added the Sign In component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import * as React from 'react';
-import ReactDOM from 'react-dom/client';
+import * as React from 'react'
+import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 
 import './index.scss'
-import App from './App';
+import App from './App'
 import reportWebvitals from './reportWebVitals'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <BrowserRouter>
@@ -16,4 +16,4 @@ root.render(
 );
 
 
-reportWebvitals();
+reportWebvitals()
