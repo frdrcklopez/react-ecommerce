27ec3b84039415bb61c42d3f41571cd04a45fb8d
effs12 <fredericklopez12@gmail.com>
2022-10-23 23:36:03 -0400

    check if user is logged and show the sign-out button

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,9 +1,19 @@
+import './navigation.styles.scss'
+import * as React from 'react'
 import {  Outlet, Link } from 'react-router-dom'
 
-import './navigation.styles.scss'
+import { signOutUser } from '../../utils/firebase/firebase.utils'
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg'
+import { UserContext } from '../../contexts/user.context'
 
 const Navigation = () => {
+    const { currentUser, setCurrentUser } = React.useContext(UserContext)
+
+    const signOutHandler = async() =>{
+        await signOutUser()
+        setCurrentUser(null)
+    }
+
     return (
         <>
             <div className='navigation'>
@@ -14,9 +24,10 @@ const Navigation = () => {
                     <Link className='nav-link' to='/shop'>
                         SHOP
                     </Link>
-                    <Link className='nav-link' to='/auth'>
-                        SIGN IN
-                    </Link>
+                    {currentUser 
+                        ? <span className='nav-link' onClick={signOutHandler}>SIGN OUT</span>
+                        : <Link className='nav-link' to='/auth'> SIGN IN </Link>
+                    }
                 </div>
             </div>
             <Outlet />
