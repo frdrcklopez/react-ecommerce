3dec8f10b5d5e1d2dccd75ab3cf828955ca2b31e
effs12 <fredericklopez12@gmail.com>
2022-10-30 23:24:30 -0400

    refactoring the sign-up  component from scss to styled component

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,9 +1,10 @@
-import './sign-up-form.styles.scss'
 import * as React from 'react'
+
 import { 
     createAuthUserWithEmailAndPassword, 
     createUserDocumentFromAuth 
 } from '../../utils/firebase/firebase.utils'
+import { SignUpContainer } from  './sign-up-form.styles.jsx'
 
 import FormInput from '../form-input/form-input.component'
 import Button from '../button/button.component'
@@ -54,7 +55,7 @@ const SignUpForm = () => {
     }
 
     return (
-        <div className='sign-up-container'>
+        <SignUpContainer>
             <h2>Don't have an account?</h2>
             <span>Sign up with your email and password</span>
             <form onSubmit={handleSubmit}>
@@ -104,7 +105,7 @@ const SignUpForm = () => {
                 
                 <Button type="submit">Sign Up</Button>
             </form>
-        </div>
+        </SignUpContainer>
     );
 }
  
