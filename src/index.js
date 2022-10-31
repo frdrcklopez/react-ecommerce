7051066f68b5d2b15cf8b066175a4d11f7578bdb
effs12 <fredericklopez12@gmail.com>
2022-10-31 17:41:18 -0400

    set the persistGate on index.js for caching data on local

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import * as React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux';
-import { store } from './store/store'
+import { PersistGate } from 'redux-persist/integration/react'
+import { persistor, store } from './store/store'
 
 import './index.scss'
 import App from './App'
@@ -13,9 +14,11 @@ const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
