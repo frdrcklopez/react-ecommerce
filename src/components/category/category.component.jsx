1d89a69e220f85f8f69270228814abf3483c96e5
effs12 <fredericklopez12@gmail.com>
2022-11-01 02:05:12 -0400

    refactoring the category store use redux-thunk and added the spinner

--- src/components/category/category.component.jsx
+++ src/components/category/category.component.jsx
@@ -6,12 +6,17 @@ import {
     CategoryContainer, 
     CategoryTitle 
 } from './category.styles.jsx'
-import { selectCategoriesMap } from '../../store/categories/category.selector.js'
+import { 
+    selectCategoriesMap, 
+    selectCategoriesIsLoading 
+} from '../../store/categories/category.selector.js'
 import ProductCard from '../product-card/product-card.component'
+import Spinner from '../spinner/spinner.component.jsx'
 
 const Category = () => {
     const { category } = useParams()
     const categoriesMap = useSelector(selectCategoriesMap)
+    const isLoading = useSelector(selectCategoriesIsLoading)
     const [products, setProducts] = useState(categoriesMap[category]) 
 
     useEffect(() => {
@@ -21,16 +26,22 @@ const Category = () => {
     return (
         <Fragment>
             <CategoryTitle>{category.toUpperCase()}</CategoryTitle>
-            <CategoryContainer>
-                {products && 
-                    products.map((product) => 
-                        <ProductCard 
-                            key={product.id}
-                            product={product}
-                        />
-                    )
-                }
-            </CategoryContainer>
+            {
+                isLoading 
+                ? <Spinner/> 
+                : (
+                    <CategoryContainer>
+                        {products && 
+                            products.map((product) => 
+                                <ProductCard 
+                                    key={product.id}
+                                    product={product}
+                                />
+                            )
+                        }
+                    </CategoryContainer>
+                )
+            }
         </Fragment>
     );
 }
