449a33aaf7ca983dc90fc9dd2d7920ed48a6fef1
frdrcklopez <fredericklopez12@gmail.com>
2022-11-02 16:48:24 -0400

    create the getCurrentUser and return the userSnapshop

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -88,7 +88,7 @@ export const createUserDocumentFromAuth = async (userAuth, additionalInformation
     }
   }
 
-  return userDocRef
+  return userSnapshot
 }
 
 export const createAuthUserWithEmailAndPassword = async (email, password) =>{
@@ -107,4 +107,17 @@ export const signOutUser = async () => await signOut(auth)
 
 export const onAuthStateChangedListener = (callback) => {
   onAuthStateChanged(auth, callback)
+}
+
+export const getCurrentUser = () => {
+  return new Promise((resolve, reject) => {
+    const unsubscribe = onAuthStateChanged(
+      auth,
+      (userAuth) => {
+        unsubscribe(); 
+        resolve(userAuth);
+      },
+      reject
+    )
+  })
 }
