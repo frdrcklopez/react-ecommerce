530fed6dcf26f9c525fc4273b73a9a6c5116380b
frdrcklopez <fredericklopez12@gmail.com>
2022-11-19 15:49:09 -0500

    fixed the action.payload white space error

--- src/store/categories/category.reducer.ts
+++ src/store/categories/category.reducer.ts
@@ -29,7 +29,7 @@ export const categoriesReducer = (
     }
 
     if(fetchCategoriesSuccess.match(action)){
-        return { ...state, categories : action. payload, isLoading : false };
+        return { ...state, categories : action.payload, isLoading : false };
     }
 
     if(fetchCategoriesFailed.match(action)){
