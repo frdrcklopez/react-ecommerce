6d3087a33e123c3b79e94db0a9dd197ba697c2fe
frdrcklopez <fredericklopez12@gmail.com>
2022-11-26 03:13:05 -0500

    apply the media query

--- src/components/category-preview/category-preview.styles.tsx
+++ src/components/category-preview/category-preview.styles.tsx
@@ -16,4 +16,12 @@ export const Preview = styled.div`
   display: grid;
   grid-template-columns: repeat(4, 1fr);
   column-gap: 20px;
+
+  @media screen and (max-width: 800px){
+    grid-template-columns: repeat(2, 1fr);
+  }
+
+  @media screen and (max-width: 400px){
+    grid-template-columns: repeat(1, 1fr);
+  }
 `
--- src/components/product-card/product-card.styles.tsx
+++ src/components/product-card/product-card.styles.tsx
@@ -7,12 +7,14 @@ export const ProductCartContainer = styled.div`
   height: 350px;
   align-items: center;
   position: relative;
+
   img {
     width: 100%;
     height: 95%;
     object-fit: cover;
     margin-bottom: 5px;
   }
+
   button {
     width: 80%;
     opacity: 0.7;
@@ -20,20 +22,35 @@ export const ProductCartContainer = styled.div`
     top: 255px;
     display: none;
   }
+
   &:hover {
     img {
       opacity: 0.8;
     }
+
     button {
       opacity: 0.85;
       display: flex;
+    }
+  }
+
+  @media screen and (max-width: 800px){
+    width: 40vw;
+
+    &:hover {
+      img {
+        opacity: unset;
+      }
+  
+      button {
+        opacity: unset;
+      }
     }
   }
 `;
 
 export const Footer = styled.div`
   width: 100%;
-  height: 5%;
   display: flex;
   justify-content: space-between;
   font-size: 18px;
@@ -41,7 +58,6 @@ export const Footer = styled.div`
 
 export const Name = styled.span`
   width: 90%;
-  margin-bottom: 15px;
 `;
 
 export const Price = styled.span`
