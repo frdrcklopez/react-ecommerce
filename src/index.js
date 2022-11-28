4953e0c27293f320c9feb7d4b04e8c6b94fc2b87
frdrcklopez <fredericklopez12@gmail.com>
2022-11-28 01:48:07 -0500

    setting up the pwa

--- src/index.js
+++ src/index.js
@@ -9,8 +9,8 @@ import { Elements } from '@stripe/react-stripe-js'
 import './index.scss'
 import App from './App'
 import { stripePromise } from './utils/stripe/stripe.utils'
-
-import reportWebvitals from './reportWebVitals'
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import reportWebVitals from './reportWebVitals'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
@@ -28,4 +28,12 @@ root.render(
 );
 
 
-reportWebvitals()
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.unregister();
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
